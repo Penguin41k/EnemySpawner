7a3bc60cdc526709c25ce12122743b9c81b2357e
Penguin41k <106864254+Penguin41k@users.noreply.github.com>
2023-06-26 18:59:34 +0500

    1

--- Assets/Skripts/Spawner.cs
+++ Assets/Skripts/Spawner.cs
@@ -7,7 +7,7 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private float _spawnTimeInterval;
     [SerializeField] private Transform _spawnPoints;
-    [SerializeField] private GameObject[] _enemy;
+    [SerializeField] private GameObject[] _enemies;
 
     private Transform[] _points;
 
@@ -29,9 +29,9 @@ public class Spawner : MonoBehaviour
 
         for (int i = 0; i < _points.Count(); i++)
         {
-            int numberOfEnemy = Random.Range(0,_enemy.Length);
+            int numberOfEnemy = Random.Range(0,_enemies.Length);
 
-            Instantiate(_enemy[numberOfEnemy], _points[i].transform.position, Quaternion.identity);
+            Instantiate(_enemies[numberOfEnemy], _points[i].transform.position, Quaternion.identity);
             yield return timer;
         }
     }
